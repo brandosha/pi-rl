d44a45d934c76ea3c0d873f8ed0f60b39bf74b13
Brandon Lantz <brandon.lantz@icloud.com>
2019-12-26 14:26:14 -0500

    Create server

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
 import * as tf from "@tensorflow/tfjs"
+import * as http from "http"
 
-tf.tensor1d([1, 4, 1]).add(tf.tensor1d([4, 1, 4])).print()
+let app = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!\n');
+})
+
+app.listen(8000, '0.0.0.0');
+
+// tf.tensor1d([1, 4, 1]).add(tf.tensor1d([4, 1, 4])).print()
